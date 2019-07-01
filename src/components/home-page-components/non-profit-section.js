d1b3857e420243ca59112bfae76a400fcb53b477
nickborbe <nickborbe@gmail.com>
2019-06-30 23:36:41 -0400

    non profit section revision

--- src/components/home-page-components/non-profit-section.js
+++ src/components/home-page-components/non-profit-section.js
@@ -15,12 +15,14 @@ class NonProfitSection extends Component {
         <section id="non-prof-section">
 
         <div id="non-prof-left-side">
-          <h2>This Section Is For Non Profit Work</h2>
+          <h2>NON-PROFIT SUPPORT</h2>
           <p>Lorem ipsum dolor amet cred yr pour-over banjo sriracha, artisan church-key cronut bicycle rights. Fanny pack quinoa sustainable, affogato unicorn cred tacos 8-bit keytar paleo hexagon. Normcore wayfarers everyday kogi. Mlkshk pug artisan cliche XOXO, vexillologist gochujang quinoa shoreditch affogato cold-pressed four loko messenger bag try-hard. Distillery yr +1, meh retro you probably haven't heard of them jean shorts lumbersexual wolf austin vape cliche irony cardigan, butcher tbh adaptogen sustainable flannel.</p>
           <button>Donate Today</button>
         </div>
 
-        <img src="/images/nonprofit-section.png" id="nonprofimage" className="hide-when-small" />
+        <div id="nonprofimage" className="hide-when-small"></div>
+
+        {/* <img src="/images/nonprofit-section.png" id="nonprofimage" className="hide-when-small" /> */}
 
         </section>
         
