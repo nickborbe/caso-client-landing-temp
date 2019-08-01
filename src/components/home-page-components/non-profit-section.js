5c1569809606a45636881abedf0e4aa028000140
nickborbe <nickborbe@gmail.com>
2019-08-01 19:04:39 -0400

    non profit section v2

--- src/components/home-page-components/non-profit-section.js
+++ src/components/home-page-components/non-profit-section.js
@@ -19,7 +19,7 @@ class NonProfitSection extends Component {
               <span>
                 <h2>Ana Maria Polo is giving back to the Humane Society for all the work they do!</h2>
                 <p>A life-long pet and animal lover, with five dogs of her own, Ana Maria Polo sought a philanthropic organization that aligns with her beliefs. The Humane Society, an American nonprofit organization, fights all forms of animal cruelty by taking on puppy mills, factory farms, trophy hunts, animal testing, and other cruel industries to achieve a humane society. Find out how Ana Maria Polo is doing her part to give back.</p>
-                <button>Donate Today</button>
+                <button id="nonprofbtn">Donate Today</button>
               </span>
             </div> 
         }
@@ -28,7 +28,7 @@ class NonProfitSection extends Component {
               <span>
                 <h2> ¡Ana Maria Polo está apoyando a la organización Humane Society por todo el trabajo que hacen!</h2>
                 <p>Ana Maria Polo ha sido una amante de las mascotas y animales durante toda su vida.  Ella vive con cinco perros los que la han motivado a buscar una organización filantrópica que se alinee con sus creencias. Humane Society es una organización estadounidense sin fines de lucro que lucha contra todas las formas de crueldad animal y enfrenta a fábricas de cachorros, granjas industriales, la práctica de cazar por trofeos, experimentos con animales y otras industrias crueles.  Su meta es lograr una sociedad más humana. Descubra lo que hace Ana Maria Polo para apoyarlos.</p>
-                <button>Donate Today</button>
+                <button id="nonprofbtn">Donate Today</button>
               </span>
             </div>
         }
