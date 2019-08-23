9f6154477cc33c05bad6019a021b293ef295fbd0
nickborbe <nickborbe@gmail.com>
2019-08-23 10:43:12 -0400

    copy on contest page

--- src/components/contest-page.js
+++ src/components/contest-page.js
@@ -47,7 +47,7 @@ class ConestPage extends Component {
         <div id="contest-left-side">
           <div className="contest-left-side-inner">
         <h2>Contest Information</h2>
-        <p>Lorem ipsum dolor amet cred yr pour-over banjo sriracha, artisan church-key cronut bicycle rights. Fanny pack quinoa sustainable, affogato unicorn cred tacos 8-bit keytar paleo hexagon. Normcore wayfarers everyday kogi. Mlkshk pug artisan cliche XOXO, vexillologist gochujang quinoa shoreditch affogato cold-pressed four loko messenger bag try-hard. Distillery yr +1, meh retro you probably haven't heard of them jean shorts lumbersexual wolf austin vape cliche irony cardigan, butcher tbh adaptogen sustainable flannel.</p>
+        <p>Ana Maria Polo contests give back to her fans that have given her so much support and motivated her for so many years! Find out more about contest rules and deadlines.</p>
         <button>REGISTER</button>
           </div>
         </div>
@@ -67,7 +67,7 @@ class ConestPage extends Component {
 
           <form>
             <input placeholder="Email Address"></input>
-            <button>Subscribe</button>
+            <button>SUBSCRIBE</button>
           </form>
 
           </section>
@@ -77,7 +77,7 @@ class ConestPage extends Component {
             
         
             <h2>JOIN THE CONVERSATION</h2>
-            <p>lorem ipsum sucre bayu losi rama weh anda re sep aluctum strea.  ave santum resitut lacerumtun ase les prolebre has ante tacitun romante lus don.90's artisan biodiesel meditation, shoreditch kinfolk beard keytar poutine fingerstache. Fingerstache humblebrag +1, aesthetic pinterest kogi celiac pop-up raclette asymmetrical shabby chic disrupt. Farm-to-table af try-hard, synth biodiesel vaporware church-key you probably haven't heard of them adaptogen lo-fi twee cold-pressed tilde occupy slow-carb. Blog woke iP</p>
+            <p>Become an Ana Maria Polo Insider. Subscribe to get access to her latest videos and news and be the first to know about exclusive contests and giveaways.</p>
         
 
             </section>
